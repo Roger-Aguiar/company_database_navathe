433014806850c747aca24817aa6e6b5883a3c36a
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-08-06 18:11:06 -0300

    Application was created inside the project and Employee model was created and populated with some data.

--- company_database/settings.py
+++ company_database/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'company_site.apps.CompanySiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- company_site/models.py
+++ company_site/models.py
@@ -1,3 +1,22 @@
+# Name:         Roger Silva Santos Aguiar
+# Function:     Models for the company_navathe database
+# Initial date: August 6, 2020
+# Last update:  August 6, 2020
+
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    fname = models.CharField('First name', max_length=15)
+    minit = models.CharField('Initial second name', max_length=1, null=False)
+    lname = models.CharField('Last name', max_length=15)
+    ssn = models.CharField('Ssn', max_length=9, primary_key=True)
+    bdate = models.DateTimeField('Birth date', null=False)
+    address = models.CharField('Address', max_length=100, null=False)
+    sex = models.CharField('Sex', max_length=1, null=False)
+    salary = models.FloatField('Salary', null=False)
+    dno = models.IntegerField('Dno')
+
+    def __str__(self):
+        employees = '{}   {}  {}   {}   {}   {}   {}   {}   {}' \
+            .format(self.fname, self.minit, self.lname, self.ssn, str(self.bdate), self.address, self.sex, str(self.salary), str(self.dno))
+        return employees
