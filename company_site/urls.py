c1e7c99aaa1800aa90ea6c1171f10e80d0d982e9
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-08-11 17:22:01 -0300

    index view was created, urls for company_site was created and a template for index view was created.

---
+++ company_site/urls.py
@@ -0,0 +1,13 @@
+# Name:         Roger Silva Santos Aguiar
+# Function:     All the views will be mapped here.
+# Initial date: August 11, 2020
+# Last update:  August 11, 2020
+
+from django.urls import path
+from . import views
+
+app_name = 'company_site'
+
+urlpatterns = [
+    path('employees/', views.index, name='index')
+]
--- company_site/views.py
+++ company_site/views.py
@@ -1,3 +1,15 @@
+# Name:         Roger Silva Santos Aguiar
+# Function:     It contains all the views for employee model
+# Initial date: August 11, 2020
+# Last update:  August 11, 2020
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, request
+from .models import Employee
 
-
+def index():
+    # employees = Employee.objects
+    employees = [Employee.fname, Employee.minit, Employee.lname, Employee.ssn, Employee.bdate, Employee.address, Employee.sex, Employee.salary, Employee.dno]
+    template = loader.get_template('company_site/index.html')
+    context = {'employees': employees, 'title': 'Employees'}
+    return HttpResponse(template.render(context, request))
